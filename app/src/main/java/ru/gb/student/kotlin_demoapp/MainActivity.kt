8332906c4cd0ff010bd182621cfde4e5f5e7d52b
Barekaev Dmitry <barekaev@bk.ru>
2021-12-24 22:55:45 +0700

    Сделан вызов object, и отображение данных из копии object в Toast Добавлен метод для вывода информации в консоль с использованием данных из цикла

--- app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
+++ app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.gb.student.kotlin_demoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,8 +16,28 @@ class MainActivity : AppCompatActivity() {
         initView()
     }
 
+    private fun showToastWithObjectInformation() {
+        val obj = User.getData()
+        val cutomObj = obj.copy("AnyName", 100)
+        Toast.makeText(this, "Default username: " + cutomObj.Name + "\nDefault age: " + cutomObj.Age, Toast.LENGTH_LONG)
+    }
+
     private fun initView() {
-        val user = UsersData("Dmitry",23);
-        findViewById<Button>(R.id.button).setOnClickListener { v -> findViewById<TextView>(R.id.textView).setText(user.Name + " " + user.Age) }
+        val user = UsersData("Dmitry",23)
+        findViewById<Button>(R.id.button).setOnClickListener { v -> findViewById<TextView>(R.id.textView).setText("Custom username: " + user.Name + "\nCustom age: " + user.Age) }
+
+        showToastWithObjectInformation()
+
+        writeInLogMessageCycle()
+    }
+
+    private fun writeInLogMessageCycle() {
+        for (i in 1..10){
+            Log.println(Log.DEBUG,"customLog", i.toString())
+        }
+
+        for (i in 25 downTo 1 step 5){
+            println("step " + i)
+        }
     }
 }
--- app/src/main/java/ru/gb/student/kotlin_demoapp/User.kt
+++ app/src/main/java/ru/gb/student/kotlin_demoapp/User.kt
@@ -1,6 +1,9 @@
 package ru.gb.student.kotlin_demoapp
 
 object User {
+    val userData = UsersData("Default User", 23)
 
-
+    fun getData(): UsersData {
+        return userData
+    }
 }
