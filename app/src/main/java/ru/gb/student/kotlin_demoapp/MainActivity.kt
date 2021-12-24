a4254f3030e23a92e5e6732ce8c2d1128e8f05cc
Barekaev Dmitry <barekaev@bk.ru>
2021-12-24 22:15:04 +0700

    Добавлена кнопка и ClickListener написанный по аналогии с Java

--- app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
+++ app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.gb.student.kotlin_demoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import ru.gb.student.kotlin_demoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        findViewById<Button>(R.id.button).setOnClickListener { v -> findViewById<TextView>(R.id.textView).setText("Button is pressed") }
     }
 }
