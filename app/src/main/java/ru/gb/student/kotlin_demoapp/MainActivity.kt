000d2759501b28f3825288087ac81f0e07f7c3c8
Barekaev Dmitry <barekaev@bk.ru>
2021-12-28 21:31:36 +0700

    Использование ViewBinding в Activity, использование простейшего рандомизатора

--- app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
+++ app/src/main/java/ru/gb/student/kotlin_demoapp/MainActivity.kt
@@ -24,24 +24,28 @@ class MainActivity : AppCompatActivity() {
     private fun showToastWithObjectInformation() {
         val obj = User.getData()
         val cutomObj = obj.copy("AnyName", 100)
-        Toast.makeText(this, "Default username: ${cutomObj.Name} Default age: ${cutomObj.Age}", Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            "Default username: ${cutomObj.Name} \nDefault age: ${cutomObj.Age}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
     private fun initView() {
-        val user = UsersData("Dmitry",23)
-        findViewById<Button>(R.id.button).setOnClickListener { v -> findViewById<TextView>(R.id.textView).setText("Custom username: ${user.Name} Custom age: ${user.Age}") }
-
+        binding.button.setOnClickListener { v ->
+            val user = UsersData("Dmitry", (18..30).random())
+            binding.textView.setText("Custom username: ${user.Name} \nCustom age: ${user.Age}")
+        }
         showToastWithObjectInformation()
-
         writeInLogMessageCycle()
     }
 
     private fun writeInLogMessageCycle() {
-        for (i in 1..10){
-            Log.println(Log.DEBUG,"customLog", i.toString())
+        for (i in 1..10) {
+            Log.println(Log.DEBUG, "customLog", i.toString())
         }
 
-        for (i in 25 downTo 1 step 5){
+        for (i in 25 downTo 1 step 5) {
             println("step $i")
         }
     }
